d1942c05ae7c7b8e12f38374f617741c2c378bab
Sungheek <klpoik@naver.com>
2018-03-15 06:37:32 +0000

    charts update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery3
 //= require turbolinks
 //= require_tree .
 //= require jquery.min
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.public_file_server.headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -29,7 +32,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += [/^[-_a-zA-Z0-9]*\..*/]
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
